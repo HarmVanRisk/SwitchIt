cbbfec7e0f5397c0d47142aa415be06b5e2dfecf
HarmVanRisk <marksharvin@gmail.com>
2017-05-29 21:18:07 +0100

    Cleaned up code and added functionality Gifs

--- SwitchIt/SwitchItExtension/SourceEditorCommand.m
+++ SwitchIt/SwitchItExtension/SourceEditorCommand.m
@@ -9,9 +9,21 @@
 #import "SourceEditorCommand.h"
 
 /*
- TODO: Handle objc enums that are all on one line
  TODO: Clean code and make it more readable
  */
+
+static NSString *const kEmptyString = @"";
+static NSString *const kEmptySpace = @" ";
+static NSString *const kCommaString = @",";
+static NSString *const kCommaSeparatorString = @",";
+static NSString *const kOpenCurlyBracket = @"{";
+static NSString *const kCloseCurlyBracket = @"}";
+static NSString *const kEqualsKey = @"=";
+static NSString *const kCaseKey = @"case ";
+static NSString *const kObjcStartOfSwitch = @"switch(<#Value#>) {\n";
+static NSString *const kObjcCaseFormat = @"%@case %@: {\n%@%@break;\n%@}\n";
+static NSString *const kSwiftStartOfSwitch = @"switch <#Value#> {\n";
+static NSString *const kSwiftCaseFormat = @"%@case .%@: \n%@%@break\n";
 
 @implementation SourceEditorCommand
 
@@ -22,7 +34,7 @@
     NSArray *selectedLines = [invocation.buffer.lines subarrayWithRange:NSMakeRange(range.start.line, length)];
     BOOL isSwiftEnum = NO;
     for (NSString *enumLine in selectedLines) {
-        if ([enumLine containsString:@"case "]) {
+        if ([enumLine containsString:kCaseKey]) {
             isSwiftEnum = YES;
             break;
         }
@@ -35,29 +47,25 @@
 }
 
 - (NSString *)expandedSwitchInObjectiveC:(XCSourceEditorCommandInvocation *)invocation withSelectedLines:(NSArray *)selectedLines {
-    NSMutableCharacterSet *setToBeTrimed = [NSMutableCharacterSet characterSetWithCharactersInString:@","];
-    [setToBeTrimed formUnionWithCharacterSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-    NSString *tabIndent = [@"" stringByPaddingToLength:invocation.buffer.tabWidth withString:@" " startingAtIndex:0];
-    NSString *switchContents = @"";
+    NSMutableCharacterSet *setToBeTrimmed = [NSMutableCharacterSet characterSetWithCharactersInString:kCommaString];
+    [setToBeTrimmed formUnionWithCharacterSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *tabIndent = [kEmptyString stringByPaddingToLength:invocation.buffer.tabWidth withString:kEmptySpace startingAtIndex:0];
+    NSString *switchContents = kEmptyString;
     for (NSString *line in selectedLines) {
-        BOOL isBeginningOrEnding = [line containsString:@"{"] || [line containsString:@"}"];
+        BOOL isBeginningOrEnding = [line containsString:kOpenCurlyBracket] || [line containsString:kCloseCurlyBracket];
         if (!isBeginningOrEnding) {
             NSString *foundCase = line;
-            foundCase = [foundCase stringByTrimmingCharactersInSet:setToBeTrimed];
-            if ([foundCase containsString:@"="]) {
-                NSRange rangeOfFoundEquals = [foundCase rangeOfString:@"="];
-                foundCase = [foundCase stringByReplacingCharactersInRange:NSMakeRange(rangeOfFoundEquals.location, foundCase.length - rangeOfFoundEquals.location) withString:@""];
-            }
+            foundCase = [foundCase stringByTrimmingCharactersInSet:setToBeTrimmed];
             if (foundCase.length > 0) {
-                switchContents = [switchContents stringByAppendingString:[NSString stringWithFormat:@"%@case %@: {\n%@%@break;\n%@}\n",tabIndent,foundCase,tabIndent,tabIndent,tabIndent]];
+                switchContents = [self switchContentsForGivenLine:foundCase withTabIndent:tabIndent totalContents:switchContents andIsSwift:NO];
             }
         }
     }
-    NSString *fullSwitch = @"";
+    NSString *fullSwitch = kEmptyString;
     if (switchContents.length > 0) {
-        fullSwitch = @"switch(<#Value#>) {\n";
+        fullSwitch = kObjcStartOfSwitch;
         fullSwitch = [fullSwitch stringByAppendingString:switchContents];
-        fullSwitch = [fullSwitch stringByAppendingString:@"}"];
+        fullSwitch = [fullSwitch stringByAppendingString:kCloseCurlyBracket];
     }
     
     return fullSwitch;
@@ -65,31 +73,63 @@
 
 - (NSString *)expandedSwitchInSwift:(XCSourceEditorCommandInvocation *)invocation withSelectedLines:(NSArray *)selectedLines {
     NSCharacterSet *setToBeTrimed = [NSCharacterSet whitespaceAndNewlineCharacterSet];
-    NSString *fullSwitch = @"switch <#Value#> {\n";
-    NSString *tabIndent = [@"" stringByPaddingToLength:invocation.buffer.tabWidth withString:@" " startingAtIndex:0];
+    NSString *tabIndent = [kEmptyString stringByPaddingToLength:invocation.buffer.tabWidth withString:kEmptySpace startingAtIndex:0];
+    NSString *switchContents = kEmptyString;
     for (NSString *line in selectedLines) {
-        BOOL isBeginningOrEnding = [line containsString:@"{"] || [line containsString:@"}"];
+        BOOL isBeginningOrEnding = [line containsString:kOpenCurlyBracket] || [line containsString:kCloseCurlyBracket];
         if (!isBeginningOrEnding) {
             NSString *foundCase = line;
-            foundCase = [foundCase stringByReplacingOccurrencesOfString:@"case " withString:@""];
+            foundCase = [foundCase stringByReplacingOccurrencesOfString:kCaseKey withString:kEmptyString];
             foundCase = [foundCase stringByTrimmingCharactersInSet:setToBeTrimed];
-            if ([foundCase containsString:@"="]) {
-                NSRange rangeOfFoundEquals = [foundCase rangeOfString:@"="];
-                foundCase = [foundCase stringByReplacingCharactersInRange:NSMakeRange(rangeOfFoundEquals.location, foundCase.length - rangeOfFoundEquals.location) withString:@""];
-            }
             if (foundCase.length > 0) {
-                if ([foundCase containsString:@","]) {
-                    for (NSString *foundSwitchOnOneLine in [foundCase componentsSeparatedByString:@", "]) {
-                        fullSwitch = [fullSwitch stringByAppendingString:[NSString stringWithFormat:@"%@case .%@: \n%@%@break\n",tabIndent,foundSwitchOnOneLine,tabIndent,tabIndent]];
-                    }
-                } else {
-                    fullSwitch = [fullSwitch stringByAppendingString:[NSString stringWithFormat:@"%@case .%@: \n%@%@break\n",tabIndent,foundCase,tabIndent,tabIndent]];
-                }
+                switchContents = [self switchContentsForGivenLine:foundCase withTabIndent:tabIndent totalContents:switchContents andIsSwift:YES];
             }
         }
     }
-    fullSwitch = [fullSwitch stringByAppendingString:@"}"];
+    NSString *fullSwitch = kEmptyString;
+    if (switchContents.length > 0) {
+        fullSwitch = kSwiftStartOfSwitch;
+        fullSwitch = [fullSwitch stringByAppendingString:switchContents];
+        fullSwitch = [fullSwitch stringByAppendingString:kCloseCurlyBracket];
+    }
     return fullSwitch;
+}
+
+- (NSString *)switchContentsForGivenLine:(NSString *)foundCase withTabIndent:(NSString *)tabIndent totalContents:(NSString *)switchContents andIsSwift:(BOOL)isSwift {
+    if ([foundCase containsString:kEqualsKey] && ![foundCase containsString:kCommaString]) {
+        //If the enum has been assigned a value we need to ignore it
+        NSRange rangeOfFoundEquals = [foundCase rangeOfString:kEqualsKey];
+        foundCase = [foundCase stringByReplacingCharactersInRange:NSMakeRange(rangeOfFoundEquals.location, foundCase.length - rangeOfFoundEquals.location) withString:kEmptyString];
+    }
+    if ([foundCase containsString:kCommaString]) {
+        //If someone by chance has put it all on one line we can still cater for it
+        switchContents = [self processMultipleCasesOnFoundLine:foundCase tabIndent:tabIndent switchContents:switchContents isSwift:isSwift];
+    } else {
+        if (isSwift) {
+            switchContents = [switchContents stringByAppendingString:[NSString stringWithFormat:kSwiftCaseFormat,tabIndent,foundCase,tabIndent,tabIndent]];
+        } else {
+            switchContents = [switchContents stringByAppendingString:[NSString stringWithFormat:kObjcCaseFormat,tabIndent,foundCase,tabIndent,tabIndent,tabIndent]];
+        }
+    }
+    return switchContents;
+}
+
+- (NSString *)processMultipleCasesOnFoundLine:(NSString *)foundCase tabIndent:(NSString *)tabIndent switchContents:(NSString *)switchContents isSwift:(BOOL)isSwift {
+    for (NSString *foundSwitchOnOneLine in [foundCase componentsSeparatedByString:kCommaSeparatorString]) {
+        NSString *otherFoundCase = foundSwitchOnOneLine;
+        otherFoundCase = [otherFoundCase stringByReplacingOccurrencesOfString:kEmptySpace withString:kEmptyString];
+        if ([otherFoundCase containsString:kEqualsKey]) {
+            //If the enum has been assigned a value we need to ignore it even when we are looping through
+            NSRange rangeOfFoundEquals = [otherFoundCase rangeOfString:kEqualsKey];
+            otherFoundCase = [otherFoundCase stringByReplacingCharactersInRange:NSMakeRange(rangeOfFoundEquals.location, otherFoundCase.length - rangeOfFoundEquals.location) withString:kEmptyString];
+        }
+        if (isSwift) {
+            switchContents = [switchContents stringByAppendingString:[NSString stringWithFormat:kSwiftCaseFormat,tabIndent,otherFoundCase,tabIndent,tabIndent]];
+        } else {
+            switchContents = [switchContents stringByAppendingString:[NSString stringWithFormat:kObjcCaseFormat,tabIndent,otherFoundCase,tabIndent,tabIndent,tabIndent]];
+        }
+    }
+    return switchContents;
 }
 
 
